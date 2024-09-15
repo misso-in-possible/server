56f56cd9609800d3ad4193ed6f9836f4880a0da3
hirossan4049 <haruto405329@gmail.com>
2024-09-16 04:22:11 +0900

    cors

--- main.py
+++ main.py
@@ -7,23 +7,26 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*", "http://localhost:*"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*", "http://localhost:*"],
+    allow_headers=["*", "http://localhost:*"],
 )
 
 currentData = 10000
 
+
 @app.get("/")
 async def read_root():
-  return "It's working"
+    return "It's working"
+
 
 @app.post("/distance-sensor")
 async def post_data(request: Request):
-  global currentData
-  body = await request.body()
-  currentData = float(body)
-  return f"ok {int(body)}"
+    global currentData
+    body = await request.body()
+    currentData = float(body)
+    return f"ok {int(body)}"
+
 
 @app.get("/distance-sensor")
 async def get_current_data():
-  return currentData
+    return currentData
